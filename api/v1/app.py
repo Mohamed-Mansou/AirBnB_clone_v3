9633967af0ecf91618b924f4423f5e3f78037923
Marwan Fouz <marwanfouz76@gmail.com>
2023-10-30 21:02:08 +0200

    update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""this module is the entry point to the REST API"""
-from flask import Flask, jsonify
-from flask_cors import CORS
+'''Contains a Flask web application API.'''
+
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """handles 404 error by returning a JSON error response"""
-    error_dict = {"error": "Not found"}
-    status_code = 404
-    return jsonify(error_dict), status_code
+def error_404(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
